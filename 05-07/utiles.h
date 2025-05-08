ac63416a190a3706e6386bb501a48098e53edee4
ailuni12 <ailunicliz@gmail.com>
2025-05-08 18:57:24 -0300

    .

--- 05-07/utiles.h
+++ 05-07/utiles.h
@@ -1,45 +1,53 @@
-#include <stdio.h>
-#include <stdlib.h>
+
 #ifndef UTILES_H_  // guardas de inclusión
 #define UTILES_H_
 
 #include <stdio.h>
 #include <conio.h>
 
-void tecla(void){
-    printf("\nPresione cualquier tecla para continuar.");
+void tecla(void)
+{
+    printf("\nPresione cualquier tecla para continuar!!!");
     getch();
 }
+void linea(int x)
+{
 
-void linea(int x){
     int i;
     for (i = 1; i < x; i++)
-        printf("-");
+        printf("*");
 }
-
-void limpiarBuffer(){
+void limpiarBuffer()
+{
     int c;
     while ((c = getchar()) != '\n' && c != EOF);
 }
-
-void utf8(){
+void utf8()
+{
     #ifdef _WIN32
       system("chcp 65001 < nul");
     #endif
 }
+#include <stdio.h>
+#include <stdlib.h>
 
-int leerEntero(const char *mensaje){
+int leerEntero(const char *mensaje)
+{
     char buffer[100];
     int numero;
     int flag = 1;
 
-    while (flag){
+    while (flag)
+    {
         printf("%s", mensaje);
-
-        if (fgets(buffer, sizeof(buffer), stdin) != NULL){
-            if (sscanf(buffer, "%d", &numero) == 1){
+        if (fgets(buffer, sizeof(buffer), stdin) != NULL)
+        {
+            if (sscanf(buffer, "%d", &numero) == 1)
+            {
                 flag = 0;
-            }else{
+            }
+            else
+            {
                 printf("Entrada inválida. Intente nuevamente.\n");
             }
         }
